04098de8e96b7b941dba9ece8a50562a0ef4d22f
yubaolee <yubaolee@163.com>
2019-06-30 22:49:09 +0800

    改造成openauth.me 的文档了 npm run bootstrap npm run dev:ssr

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ if (isSSR) {
   <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <title>docsify</title>
+    <title>OpenAuth.Net文档--最好用的.NET权限工作流框架</title>
+    <meta name="Keywords" content="openauth.net如何部署,.net开源工作流,OpenAuth.Net文档">
+<meta name="Description" content="OpenAuth.Net文档,最好用的.net开源工作流框架,最好用的.NET开源权限管理框架">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="/themes/vue.css" title="vue">
   </head>
@@ -18,22 +20,28 @@ if (isSSR) {
     <!--inject-app-->
     <!--inject-config-->
   <script src="/lib/docsify.js"></script>
+  <script>
+  //百度统计
+  var _hmt = _hmt || [];
+  (function() {
+      var hm = document.createElement("script");
+      hm.src = "//hm.baidu.com/hm.js?0558502420ce5fee054b31425e77ffa6";
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(hm, s);
+  })();
+</script>
   </body>
   </html>`,
     config: {
-      name: 'docsify',
-      repo: 'qingwei-li/docsify',
-      basePath: 'https://docsify.js.org/',
-      loadNavbar: true,
+      name: 'OpenAuth.Core',
+      repo: 'yubaolee/OpenAuth.Core',
+    //  basePath: 'http://doc.openauth.me/',  //用gitee pages会有跨域的问题😰,已经把这个域名映射到主站了
+     basePath: 'https://yubaolee.github.io/OpenAuth.Core/',
+    //  basePath: 'https://docsify.js.org/',
+      loadNavbar: false,
       loadSidebar: true,
-      subMaxLevel: 3,
-      auto2top: true,
-      alias: {
-        '/de-de/changelog': '/changelog',
-        '/zh-cn/changelog': '/changelog',
-        '/changelog':
-          'https://raw.githubusercontent.com/QingWei-Li/docsify/master/CHANGELOG'
-      }
+      subMaxLevel: 4,
+      auto2top: true
     },
     path: './'
   })
@@ -44,6 +52,7 @@ if (isSSR) {
     }
     renderer.renderToString(req.url).then(html => res.end(html))
   })
+
 }
 
 const params = {
